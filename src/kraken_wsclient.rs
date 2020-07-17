2e4a583eb2d0ae5f47dec454dca5b92cbe972f17
StefanMallia <5004438+StefanMallia@users.noreply.github.com>
2020-07-17 22:47:16 +0200

    Changed from using ws::connect to Builder

--- src/kraken_wsclient.rs
+++ src/kraken_wsclient.rs
@@ -13,23 +13,32 @@ pub fn connect(asset_pairs_vec: std::vec::Vec<String>,
                subscriptions_vec: std::vec::Vec<String>,//,
                mut observers: Vec<Box<dyn Observer>>)
 {
-    let _result = ws::connect("wss://ws.kraken.com",
-        move |ws_out: ws::Sender|
-        {
-            println!("Connected to websocket.");
-            // work around for transferring ownership of Observers from this closure
-            // to Client
-            let mut to_use_observers: Vec<Box<dyn Observer>> = vec![];
-            while observers.len() > 0
-            {
-                to_use_observers.push(observers.pop().unwrap());
-            }
-            Client{ws_out: ws_out, observers: to_use_observers,
-                   asset_pairs_vec: asset_pairs_vec.clone(),
-                   subscriptions_vec: subscriptions_vec.clone(),
-                   last_update: std::time::Instant::now()}
-        });
 
+    //: fn(ws::Sender) -> Client
+    let factory =
+        move |ws_out: ws::Sender|
+            {
+                println!("Connected to websocket.");
+                // work around for transferring ownership of Observers from this closure
+                // to Client
+                let mut to_use_observers: Vec<Box<dyn Observer>> = vec![];
+                while observers.len() > 0
+                {
+                    to_use_observers.push(observers.pop().unwrap());
+                }
+                Client{ws_out: ws_out, observers: to_use_observers,
+                    asset_pairs_vec: asset_pairs_vec.clone(),
+                    subscriptions_vec: subscriptions_vec.clone(),
+                    last_update: std::time::Instant::now()}
+            };
+    let settings = ws::Settings{
+        ..Default::default()
+    };
+    let mut websocket = ws::Builder::new().with_settings(settings).build(factory).unwrap();
+
+
+    websocket.connect(url::Url::parse("wss://ws.kraken.com").unwrap());
+    websocket.run();
 
 }
 
@@ -42,19 +51,31 @@ struct Client
     last_update: std::time::Instant
 }
 
-impl Client
-{
-    fn attempt_connect(&mut self)
-    {
-        println!("Attempting connect.");
-        let result= self.ws_out.connect(url::Url::parse("wss://ws.kraken.com").unwrap());
-        match result
-        {
-            Ok(T) => T,
-            Err(E) => {println!("Connection failed, retrying."); self.attempt_connect();}
-        }
-    }
-}
+
+// impl io::Handler for Client
+// {
+//     fn random_function(&mut self)
+//     {
+//         self.run();
+//
+//     }
+//
+// }
+// impl Client
+// {
+//     fn attempt_connect(&mut self)
+//     {
+//         println!("Attempting connect.");
+//         let result= self.ws_out.connect(url::Url::parse("wss://ws.kraken.com").unwrap());
+//
+//
+//         match result
+//         {
+//             Ok(T) => {println!("Got Ok on connect");},
+//             Err(E) => {println!("Connection failed, retrying."); self.attempt_connect();}
+//         }
+//     }
+// }
 
 impl ws::Handler for Client
 {
@@ -85,45 +106,48 @@ impl ws::Handler for Client
     fn on_timeout(&mut self, _event: ws::util::Token) -> ws::Result<()>
     {
         println!("Timed out");
-        //let mut to_transfer_observers: Vec<Box<dyn Observer>> = vec![];
-        //while self.observers.len() > 0
-        //{
-        //    to_transfer_observers.push(self.observers.pop().unwrap());
-        //}
-        //connect(self.asset_pairs_vec.clone(), self.subscriptions_vec.clone(),
-        //             to_transfer_observers);
-        self.attempt_connect();
+        let mut to_transfer_observers: Vec<Box<dyn Observer>> = vec![];
+        while self.observers.len() > 0
+        {
+           to_transfer_observers.push(self.observers.pop().unwrap());
+        }
+        connect(self.asset_pairs_vec.clone(), self.subscriptions_vec.clone(),
+                    to_transfer_observers);
+        // self.attempt_connect();
         Ok(())
     }
 
     fn on_error(&mut self, error: ws::Error)
     {
         println!("Error: {}", error);
-        //let mut to_transfer_observers: Vec<Box<dyn Observer>> = vec![];
-        //while self.observers.len() > 0
-        //{
-        //    to_transfer_observers.push(self.observers.pop().unwrap());
-        //}
-        //connect(self.asset_pairs_vec.clone(), self.subscriptions_vec.clone(),
-        //             to_transfer_observers);
+        let mut to_transfer_observers: Vec<Box<dyn Observer>> = vec![];
+        while self.observers.len() > 0
+        {
+           to_transfer_observers.push(self.observers.pop().unwrap());
+        }
+        connect(self.asset_pairs_vec.clone(), self.subscriptions_vec.clone(),
+                    to_transfer_observers);
 
-        self.attempt_connect()
+        // self.attempt_connect();
+
     }
     fn on_close(&mut self, code: ws::CloseCode, reason: &str)
     {
-        println!("Closing: {}", reason);
-        //let mut to_transfer_observers: Vec<Box<dyn Observer>> = vec![];
-        //while self.observers.len() > 0
-        //{
-        //    to_transfer_observers.push(self.observers.pop().unwrap());
-        //}
-        //connect(self.asset_pairs_vec.clone(), self.subscriptions_vec.clone(),
-        //             to_transfer_observers);
+        let code_int: u16 = code.into();
+        println!("Closing: {} {}", reason, code_int);
+        let mut to_transfer_observers: Vec<Box<dyn Observer>> = vec![];
+        while self.observers.len() > 0
+        {
+           to_transfer_observers.push(self.observers.pop().unwrap());
+        }
+        connect(self.asset_pairs_vec.clone(), self.subscriptions_vec.clone(),
+                    to_transfer_observers);
 
-        self.attempt_connect()
+        // self.attempt_connect()
         
 
     }
+
     fn on_message(&mut self, msg: ws::Message) -> ws::Result<()>
     {
         // let msg2: ws::Message = msg.clone();
@@ -166,6 +190,7 @@ impl ws::Handler for Client
                 }
             }
             self.last_update = std::time::Instant::now();
+
 
         }
         // (self.observers[0])(msg2);
