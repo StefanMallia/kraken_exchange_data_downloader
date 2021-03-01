1ba6fe6ec718baa0b758bb4d52211f518425012d
StefanMallia <stefanmallia@gmail.com>
2021-03-01 22:52:26 +0100

    cleanup

--- src/store_kraken_to_text.rs
+++ src/store_kraken_to_text.rs
@@ -3,12 +3,11 @@ use std::fs::File;
 use std::fs::OpenOptions;
 use std::io::Write;
 
-
-
 pub struct TextFile
 {
     pub output_directory: String,
 }
+
 impl TextFile
 {
     pub fn new(output_directory: &str) -> TextFile
@@ -20,20 +19,16 @@ impl TextFile
     pub fn append_to_file(&mut self, file_name: &str,
                          kraken_messages: &Vec<Box<json::JsonValue>>)
     {
-
         let mut file: File
             = OpenOptions::new().create(true).append(true).open([self.output_directory.to_string(),
                                                                    "/".to_string(),
                                                                    file_name.to_string()].concat()
                                                              ).expect("Can't open file.");
-
         for kraken_message in kraken_messages
         {
             file.write_all([kraken_message.to_string(),
                             "\n".to_string()
                            ].concat().as_bytes()).expect("write failed");
         }
-
     }
-
 }
